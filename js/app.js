defa9e7bbc3af31cdb133cea1f9c64168a7c41dc
bymoen <alexb@innotechcollege.com>
2023-03-16 17:44:03 -0600

    Added a simple conditional check

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let username = "best_user";
+let user_age = 30;
+let is_member = true;
+
+if(username == 'admin' || (user_age >= 30 && is_member === true)) {
+    console.log("Special Greeting");
+} else {
+    console.log("Standard Greeting");
+}
